754ad63bd12a5dfbe5275f010eaa1c7c54da30ef
Grisha <grisha765@tuta.io>
2025-05-27 03:07:59 +0300

    [+] init autotests

---
+++ appeals/tests/test_ping.py
@@ -0,0 +1,132 @@
+import asyncio
+from pyrogram.client import Client
+
+from appeals.funcs.ping import ping_command
+
+from appeals.config.config import Config
+from appeals.config import logging_config
+
+
+async def ping_reset(logger, app):
+    try:
+        msg = await app.send_message(
+            chat_id=Config.test_chat_id,
+            text="/ping reset"
+        )
+        await ping_command(app, msg)
+
+        out_msg = await app.get_messages(
+            chat_id=Config.test_chat_id,
+            message_ids=msg.id + 1
+        )
+        assert out_msg.text == "🏓PONG!\nResp: {'Pong': '0'}", f"Expected '0', got {msg.text!r}"
+
+        logger.info("Test passed! #1 (ping reset)")
+    except AssertionError as e:
+        logger.info(f"Test failed! #1 (ping reset): {e}")
+        raise
+
+
+async def ping_plus(logger, app):
+    try:
+        msg = await app.send_message(
+            chat_id=Config.test_chat_id,
+            text="/ping plus"
+        )
+        await ping_command(app, msg)
+
+        out_msg = await app.get_messages(
+            chat_id=Config.test_chat_id,
+            message_ids=msg.id + 1
+        )
+        assert out_msg.text == "🏓PONG!\nResp: {'Pong': '1'}", f"Expected '1', got {msg.text!r}"
+
+        logger.info("Test passed! #2 (ping plus)")
+    except AssertionError as e:
+        logger.info(f"Test failed! #2 (ping plus): {e}")
+        raise
+
+
+async def ping_minus(logger, app):
+    try:
+        msg = await app.send_message(
+            chat_id=Config.test_chat_id,
+            text="/ping minus"
+        )
+        await ping_command(app, msg)
+
+        out_msg = await app.get_messages(
+            chat_id=Config.test_chat_id,
+            message_ids=msg.id + 1
+        )
+        assert out_msg.text == "🏓PONG!\nResp: {'Pong': '0'}", f"Expected '0', got {msg.text!r}"
+
+        logger.info("Test passed! #3 (ping minus)")
+    except AssertionError as e:
+        logger.info(f"Test failed! #3 (ping minus): {e}")
+        raise
+
+
+async def ping_set(logger, app):
+    try:
+        msg = await app.send_message(
+            chat_id=Config.test_chat_id,
+            text="/ping set 42"
+        )
+        await ping_command(app, msg)
+
+        out_msg = await app.get_messages(
+            chat_id=Config.test_chat_id,
+            message_ids=msg.id + 1
+        )
+        assert out_msg.text == "🏓PONG!\nResp: {'Pong': '42'}", f"Expected '42', got {msg.text!r}"
+
+        logger.info("Test passed! #4 (ping set)")
+    except AssertionError as e:
+        logger.info(f"Test failed! #4 (ping set): {e}")
+        raise
+
+
+async def ping_get(logger, app):
+    try:
+        msg = await app.send_message(
+            chat_id=Config.test_chat_id,
+            text="/ping"
+        )
+        await ping_command(app, msg)
+
+        out_msg = await app.get_messages(
+            chat_id=Config.test_chat_id,
+            message_ids=msg.id + 1
+        )
+        assert out_msg.text == "🏓PONG!\nCount: 42", f"Expected '42', got {msg.text!r}"
+
+        logger.info("Test passed! #5 (ping get)")
+    except AssertionError as e:
+        logger.info(f"Test failed! #5 (ping get): {e}")
+        raise
+
+
+async def test_ping():
+    logger = logging_config.setup_logging(__name__)
+    test_app = Client(
+        name="dummy",
+        api_id=Config.tg_id,
+        api_hash=Config.tg_hash,
+        session_string=Config.test_session,
+        in_memory=True
+    )
+    async with test_app as app:
+        await ping_reset(logger, app)
+        await asyncio.sleep(1)
+        await ping_plus(logger, app)
+        await asyncio.sleep(1)
+        await ping_minus(logger, app)
+        await asyncio.sleep(1)
+        await ping_set(logger, app)
+        await asyncio.sleep(1)
+        await ping_get(logger, app)
+
+
+if __name__ == "__main__":
+    raise RuntimeError("This module should be run only via main.py")

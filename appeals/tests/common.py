c20f55d00f1d2ff6b52653b97245c6d91116ec8f
Grisha <grisha765@tuta.io>
2025-05-28 01:29:43 +0300

    [+] common functions and classes for tests

---
+++ appeals/tests/common.py
@@ -0,0 +1,33 @@
+class DummyCallbackQuery:
+    def __init__(self, *, msg, data):
+        self.id = "local-test"
+        self.from_user = msg.from_user
+        self.chat_instance = "dummy"
+        self.message = msg
+        self.data = data
+
+    async def answer(self, *args, **kwargs):
+        return True
+
+def get_callback_data(
+        message,
+        target_text: str
+):
+    kb = getattr(
+        message.reply_markup,
+        "inline_keyboard",
+        None
+    )
+    if kb is None:
+        return None
+
+    for row in kb:
+        for button in row:
+            if button.text == target_text:
+                return button.callback_data
+
+    return None
+
+
+if __name__ == "__main__":
+    raise RuntimeError("This module should be run only via main.py")

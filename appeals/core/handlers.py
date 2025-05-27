0c6e0dd30bc85bd9baea4b32a07d8a4fb592d784
Grisha <grisha765@tuta.io>
2025-05-27 20:47:00 +0300

    [u] add handlers

--- appeals/core/handlers.py
+++ appeals/core/handlers.py
@@ -1,6 +1,11 @@
 import pyrogram.filters
 import pyrogram.handlers.message_handler
 from appeals.funcs.ping import ping_command
+from appeals.funcs.start import start_command
+from appeals.funcs.conversion import (
+        create_conversion,
+        create_conversion_text
+)
 
 
 def init_handlers(app):
@@ -8,6 +13,26 @@ def init_handlers(app):
         pyrogram.handlers.message_handler.MessageHandler(
             ping_command,
             pyrogram.filters.command("ping") &
+                pyrogram.filters.private
+        )
+    )
+    app.add_handler(
+        pyrogram.handlers.message_handler.MessageHandler(
+            start_command,
+            pyrogram.filters.command("start") &
+                pyrogram.filters.private
+        )
+    )
+    app.add_handler(
+        pyrogram.handlers.callback_query_handler.CallbackQueryHandler(
+            create_conversion,
+            pyrogram.filters.regex("^create_conversion$")
+        )
+    )
+    app.add_handler(
+        pyrogram.handlers.message_handler.MessageHandler(
+            create_conversion_text,
+            pyrogram.filters.text &
                 pyrogram.filters.private
         )
     )

e59fc32d2a61356cba9c0e31ca27ad4eb380d495
Grisha <grisha765@tuta.io>
2025-05-27 20:44:56 +0300

    [u] new vars

--- appeals/core/common.py
+++ appeals/core/common.py
@@ -1,8 +1,17 @@
 import httpx
+from pyrogram.types import InlineKeyboardButton
 
 
 class Common():
     http = httpx.AsyncClient(timeout=30)
+    waiting_for_input = {}
+
+
+class Buttons():
+    create_conversion = InlineKeyboardButton(
+        text="📄 Написать обращение 📄",
+        callback_data="create_conversion"
+    )
 
 
 if __name__ == "__main__":

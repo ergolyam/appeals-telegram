7541fe10db2d3c52aa762671a75f927e9a5e72ed
Grisha <grisha765@tuta.io>
2025-05-27 21:57:50 +0300

    [u] optimization for callback_query

--- appeals/funcs/start.py
+++ appeals/funcs/start.py
@@ -2,17 +2,34 @@ from pyrogram.types import InlineKeyboardMarkup
 from appeals.core.common import Buttons, Common
 
 
-async def start_command(_, message):
+async def start_command(message):
     user = message.from_user
     if Common.waiting_for_input.get(user.id):
         return
     buttons = []
-    buttons.append([Buttons.create_conversion])
-    await message.reply_text(
-        text="Здравствуйте! Выберите действие:",
-        reply_markup=InlineKeyboardMarkup(buttons)
-    )
+    buttons.append([Buttons.create_conversion, Buttons.conversions_list])
+    if getattr(message, 'data', None) is not None:
+        await message.message.edit_text(
+            text="Здравствуйте! Выберите действие:",
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    else:
+        await message.reply_text(
+            text="Здравствуйте! Выберите действие:",
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
 
+
+async def start_msg(_, message):
+    await start_command(message)
+
+
+async def start_cb(_, callback_query):
+    user = callback_query.from_user
+    state = Common.waiting_for_input.get(user.id)
+    if state:
+        Common.waiting_for_input.pop(user.id, None)
+    await start_command(callback_query)
 
 
 if __name__ == "__main__":
